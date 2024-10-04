3a3fb12d08c578e1b083cbcf67e24f2c382400d8
maskwel <57584511+maskwel@users.noreply.github.com>
2024-10-04 13:51:01 -0700

    basic js Assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,6 @@
+document.write("Hello, World! ");
+
+var sent1 = "This is the beginning of the string";
+var sent2 = "and this is the end of the string";
+
+document.write(sent1+sent2);
---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,9 @@
+function Count_Function() {
+    document.getElementById("count_result").innerHTML = Count();
+    function Count() {
+        var Starting_point = 9;
+        function Plus_one() {Starting_point += 1;}
+        Plus_one();
+        return Starting_point;
+    }
+}
